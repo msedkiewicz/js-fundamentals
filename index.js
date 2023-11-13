1d9de6e8bdfe2e08ec7ee409f1bce402d4e9cd88
msedkiewicz <magdalenainspirations@gmail.com>
2023-11-13 20:46:57 +0100

    Naming convention in JS

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-let name;
+let name = 'Lena';
 console.log(name);
 
-// by default variables defined in JS have value = undefined
+// Cannot be a reserved keyword
+// Should be meaningful and descriptive
+// Cannot start with a number
+// Cannot contain a space or hyphen (-) - use Camel notation instead
+// Are case-sensitive
