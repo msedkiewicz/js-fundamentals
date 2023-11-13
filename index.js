87a6b7b4810266e29e29fdeae524df43fde9f8be
msedkiewicz <magdalenainspirations@gmail.com>
2023-11-13 20:48:40 +0100

    Initializing variables

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ console.log(name);
 // Cannot start with a number
 // Cannot contain a space or hyphen (-) - use Camel notation instead
 // Are case-sensitive
+
+// Initializing
+let manchesterUnited, manchesterCity;
+let astonVilla = 'Unai Emery', arsenal = 'Mikel Arteta';
+
+// Best practice for initializing
+let chelsea = 'Mauricio Pochettino';
+let newcastleUnited = 'Eddie Howe';
+let year = '2023';
