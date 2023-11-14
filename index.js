8523bc409b5063dccbc2e6d03929c2312fdbaffd
msedkiewicz <magdalenainspirations@gmail.com>
2023-11-14 14:31:18 +0100

    Constant and primitives notes

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ let astonVilla = 'Unai Emery', arsenal = 'Mikel Arteta';
 let chelsea = 'Mauricio Pochettino';
 let newcastleUnited = 'Eddie Howe';
 let year = '2023';
+
+// if there is no need to reassign value - use const; otherwise, use let
+const myName = 'Lena';
+let group = 'Gryffindor';
+
+// Primitives / Value Types
+let nickname = 'Snakebite'; // String Literal
+let age = 21; // Number Literal
+let isApproved = true; // Boolean Literal
+let firstName; // undefined
+let lastName = null; // for clearing the value of variable
+
+// Reference Types
