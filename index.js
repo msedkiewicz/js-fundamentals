68c8a9c744c75ed409e926733ab3b81a810031c5
msedkiewicz <magdalenainspirations@gmail.com>
2023-11-25 22:21:35 +0100

    if else statement example

---
+++ index.js
@@ -0,0 +1,12 @@
+// If... else
+let hour = 10;
+
+if (hour < 6) {
+    console.log('You should be sleeping!');
+} else if (hour >=6 && hour <12) {
+    console.log('Good morning!');
+} else if (hour >=12 && hour < 18) {
+    console.log('Good afternoon!');
+} else {
+    console.log('Good evening!');
+}
---
+++ notes.js
@@ -0,0 +1,11 @@
+// Conditional statements
+// if...else
+// switch... case
+
+if (condition) {
+    statement;
+} else if (!condition) {
+    anotherStatement;
+} else {
+    somethingGeneric;
+}
