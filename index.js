532c7a7e86d3df79585b9ab6d40c4a646be3a23d
msedkiewicz <magdalenainspirations@gmail.com>
2023-11-25 22:22:12 +0100

    simplified if else

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 // If... else
 let hour = 10;
 
-if (hour < 6) {
-    console.log('You should be sleeping!');
-} else if (hour >=6 && hour <12) {
-    console.log('Good morning!');
-} else if (hour >=12 && hour < 18) {
-    console.log('Good afternoon!');
-} else {
-    console.log('Good evening!');
-}
+if (hour < 6) console.log("You should be sleeping!");
+else if (hour >= 6 && hour < 12) console.log("Good morning!");
+else if (hour >= 12 && hour < 18) console.log("Good afternoon!");
+else console.log("Good evening!");
