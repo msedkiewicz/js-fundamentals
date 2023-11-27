899256577f5ec2ede2f0b827c39160ed62a7e6cf
msedkiewicz <magdalenainspirations@gmail.com>
2023-11-27 22:58:04 +0100

    Demerit points exercise - my solution

--- index.js
+++ index.js
@@ -1,47 +1,26 @@
-// Divisible by 3 => Fizz
-// Divisible by 5 => Buzz
-// Divisible by 3 and 5 => FizzBuzz
-// other cases => return number
-// Not a number => return 'not a number' phrase
+// Speed limit - 70
+// every 5 km over - 1 point
+// if driver gets more than 12 points - driver licence is suspended
 
-const output1 = fizzBuzz(3);
-const output2 = fizzBuzz(5);
-const output3 = fizzBuzz(15);
-const output4 = fizzBuzz(7);
-const output5 = fizzBuzz(true);
-console.log(
-  "Oputput1:",
-  output1,
-  "Oputput2:",
-  output2,
-  "Oputput3:",
-  output3,
-  "Oputput4:",
-  output4,
-  "Oputput5:",
-  output5
-);
-
-// function fizzBuzz(input) {
-//     if (input % 3 === 0 && input % 5 === 0) {
-//         return 'FizzBuzz';
-//     } else if (input % 3 === 0) {
-//         return 'Fizz';
-//     } else if (input % 5 === 0) {
-//         return 'Buzz';
-//     } else if (typeof input === 'number') {
-//         return input;
-//     } else {
-//         return 'Not a number';
-//     }
-// }
-
-// course solution:
-
-function fizzBuzz(input) {
-    if (typeof input !== 'number') return NaN;
-    if ((input % 3 === 0) && (input % 5 === 0)) return 'FizzBuzz';
-    if (input % 3 === 0) return 'Fizz';
-    if (input % 5 === 0) return 'Buzz';
-    return input;
+console.log(checkSpeed(50));
+console.log(checkSpeed(70));
+console.log(checkSpeed(75));
+console.log(checkSpeed(74));
+console.log(checkSpeed(98));
+console.log(checkSpeed(150));
+function checkSpeed(speed) {
+    if (typeof speed === 'number') {
+        if (speed <= 70) {
+            return speed;
+        } else if (speed > 70) {
+            const points = Math.floor((speed - 70) / 5);
+            if (points === 0) {
+                return speed;
+            } else if (points > 0 && points < 12) {
+                return points;
+            } else {
+                return 'License suspended';
+            }
+        }
+    }
 }
