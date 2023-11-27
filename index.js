126df0cd733b10d03c528f490c3ecea5802b233a
msedkiewicz <magdalenainspirations@gmail.com>
2023-11-27 23:10:01 +0100

    Code refactoring

--- index.js
+++ index.js
@@ -8,16 +8,21 @@ console.log(checkSpeed(75));
 console.log(checkSpeed(74));
 console.log(checkSpeed(98));
 console.log(checkSpeed(150));
+console.log(checkSpeed(130));
+
 function checkSpeed(speed) {
     if (typeof speed === 'number') {
-        if (speed <= 70) {
+        const speedLimit = 70;
+        const kmPerPoint = 5;
+        const penaltyPoints = 12;
+        if (speed <= speedLimit) {
             return speed;
-        } else if (speed > 70) {
-            const points = Math.floor((speed - 70) / 5);
+        } else {
+            const points = Math.floor((speed - speedLimit) / kmPerPoint);
             if (points === 0) {
                 return speed;
-            } else if (points > 0 && points < 12) {
-                return points;
+            } else if (points > 0 && points < penaltyPoints) {
+                return `Points: ${points}`;
             } else {
                 return 'License suspended';
             }
