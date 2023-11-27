6870185f3a99c3c5af3d13db0b0678d855243349
msedkiewicz <magdalenainspirations@gmail.com>
2023-11-27 12:46:52 +0100

    code refactoring +tests

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+console.log(isLandscape(100,200));
+console.log(isLandscape(200,100));
+console.log(isLandscape(200,200));
+
 function isLandscape(width, height) {
-  return (width > height) ? true : false;
+  return (width > height); // function already returns true or false
 }
