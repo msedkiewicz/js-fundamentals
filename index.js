5e6b09dbba24b57b147ff65ba70c6c54f08c15e7
msedkiewicz <magdalenainspirations@gmail.com>
2023-11-27 23:19:14 +0100

    code refactoring

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 showNumbers(10);
 
 function showNumbers(limit) {
-  for (i = 0; i <= limit; i++) {
-    if (i % 2 !== 0) {
-      console.log(i, "ODD");
-    } else {
-      console.log(i, "EVEN");
-    }
+  for (let i = 0; i <= limit; i++) {
+    if (i % 2 !== 0) console.log(i, "ODD");
+    else console.log(i, "EVEN");
   }
 }
