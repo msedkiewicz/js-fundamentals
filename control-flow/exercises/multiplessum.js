64899950ba0cf8b9e04e4b41a34e239f683e2ba2
msedkiewicz <magdalenainspirations@gmail.com>
2023-11-28 22:28:19 +0100

    Grade - exercise

---
+++ control-flow/exercises/multiplessum.js
@@ -0,0 +1,16 @@
+// Multiples of 3: 3, 6, 9
+// Multiples of 5: 5, 10
+
+console.log(sum(10));
+
+function sum(limit) {
+  let multiplesSum = 0;
+
+  for (let i = 0; i <= limit; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      multiplesSum += i;
+    }
+  }
+
+  return multiplesSum;
+}
--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-// Multiples of 3: 3, 6, 9
-// Multiples of 5: 5, 10
+// 1-59: F
+// 60-69: D
+// 70-79: C
+// 80-89: B
+// 90-100: A
 
-console.log(sum(10));
+const studentsMarks1 = [50, 40, 50];
+const studentsMarks2 = [60, 68, 65];
+const studentsMarks3 = [80, 80, 50];
+const studentsMarks4 = [80, 80, 95];
+const studentsMarks5 = [95, 95, 100];
 
-function sum(limit) {
-  let multiplesSum = 0;
+function calculateGrade(marks) {
+  let sumOfMarks = 0;
 
-  for (let i = 0; i <= limit; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      multiplesSum += i;
-    }
+  for(const mark of marks) {
+    sumOfMarks += mark;
   }
 
-  return multiplesSum;
+  const average = sumOfMarks / marks.length;
+
+  if (average <= 59) {
+    console.log('F');
+  } else if (average > 59 && average <= 69) {
+    console.log('D');
+  } else if (average > 69 && average <= 79) {
+    console.log('C');
+  } else if (average > 79 && average <= 89) {
+    console.log('B');
+  } else if (average > 89 && average <= 100) {
+    console.log('A');
+  }
 }
+
+calculateGrade(studentsMarks1);
+calculateGrade(studentsMarks2);
+calculateGrade(studentsMarks3);
+calculateGrade(studentsMarks4);
+calculateGrade(studentsMarks5);
