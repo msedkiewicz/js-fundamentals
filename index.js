ecec523e3fb57641e8de900879c655abda0713c3
msedkiewicz <magdalenainspirations@gmail.com>
2023-11-28 22:35:27 +0100

    Code refactoring

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 1-59: F
+// 0-59: F
 // 60-69: D
 // 70-79: C
 // 80-89: B
@@ -13,27 +13,21 @@ const studentsMarks5 = [95, 95, 100];
 function calculateGrade(marks) {
   let sumOfMarks = 0;
 
-  for(const mark of marks) {
+  for (const mark of marks) {
     sumOfMarks += mark;
   }
 
   const average = sumOfMarks / marks.length;
 
-  if (average <= 59) {
-    console.log('F');
-  } else if (average > 59 && average <= 69) {
-    console.log('D');
-  } else if (average > 69 && average <= 79) {
-    console.log('C');
-  } else if (average > 79 && average <= 89) {
-    console.log('B');
-  } else if (average > 89 && average <= 100) {
-    console.log('A');
-  }
+  if (average < 60) return "F";
+  if (average < 70) return "D";
+  if (average < 80) return "C";
+  if (average < 90) return "B";
+  return "A";
 }
 
-calculateGrade(studentsMarks1);
-calculateGrade(studentsMarks2);
-calculateGrade(studentsMarks3);
-calculateGrade(studentsMarks4);
-calculateGrade(studentsMarks5);
+console.log(calculateGrade(studentsMarks1));
+console.log(calculateGrade(studentsMarks2));
+console.log(calculateGrade(studentsMarks3));
+console.log(calculateGrade(studentsMarks4));
+console.log(calculateGrade(studentsMarks5));
