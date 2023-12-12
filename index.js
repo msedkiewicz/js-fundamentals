1dd4411dbb7e76a9feafe44eb876ccf7a595d2b3
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-12 15:19:26 +0100

    Code refactoring

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 // Prime and Composite numbers - composite has many factors, prime only 1 and prime number itself
 
 function showPrimes(limit) {
-  for (let number = 2; number <= limit; number++) {
-    let isPrime = true;
-    for (let factor = 2; factor < number; factor++) {
-      if (number % factor === 0) {
-        isPrime = false;
-        break;
-      }
-    }
+  for (let number = 2; number <= limit; number++)
+    if (isPrime(number)) console.log(number);
+}
 
-    if (isPrime) console.log(number);
-  }
+function isPrime(number) {
+  for (let factor = 2; factor < number; factor++)
+    if (number % factor === 0) return false;
+  return true;
 }
 
 showPrimes(20);
