5c226b343144a3c61e5f317cb5e007874f082c84
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-12 16:59:59 +0100

    Constructor function example

--- notes.js
+++ notes.js
@@ -24,10 +24,11 @@ const circle = {
   },
 };
 
-circle.draw(); // Method
+// circle.draw(); // Method
 
 // Factory function
 function createCircle(radius) {
+  // camelCase notation / camel notation
   return {
     radius,
     draw() {
@@ -36,8 +37,20 @@ function createCircle(radius) {
   };
 }
 
-const circle1 = createCircle(1);
-console.log(circle1);
-circle1.draw();
-const circle2 = createCircle(2);
-console.log(circle2);
+// const circle1 = createCircle(1);
+// console.log(circle1);
+// circle1.draw();
+// const circle2 = createCircle(2);
+// console.log(circle2);
+
+// Constructor function
+
+function Circle(radius) {
+  // classes start with big letter :P
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
+
+const circle3 = new Circle(1);
