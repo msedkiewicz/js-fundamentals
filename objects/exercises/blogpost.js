41e661b150d75106ceebf9bd3e1bebab2e277996
msedkiewicz <magdalenainspirations@gmail.com>
2024-06-14 20:59:55 +0200

    blog post object exercise

---
+++ objects/exercises/blogpost.js
@@ -0,0 +1,14 @@
+let post = {
+  title: "Hello World!",
+  body: "Lorem ipsum",
+  author: "Lena Sedkiewicz",
+  views: 10,
+  comments: [
+    { author: "a", body: "b" },
+    { author: "c", body: "d" },
+    { author: "e", body: "r" },
+  ],
+  isLive: true,
+};
+
+console.log(post);
