ab66466f504f5ba073f427bd000d4b225c26339a
msedkiewicz <magdalenainspirations@gmail.com>
2024-06-19 23:47:14 +0200

    Notes on emptying an array

---
+++ arrays/notes/emptyingarray.js
@@ -0,0 +1,29 @@
+let numbers = [1, 2, 3, 4, 5];
+let another = numbers;
+
+// Solution 1
+numbers = [];
+console.log(numbers);
+console.log(another);
+
+//Solution 2
+
+let luckynumbers = [1, 2, 3, 4, 5];
+let anotherlucky = luckynumbers;
+
+luckynumbers.length = 0;
+console.log(luckynumbers);
+console.log(anotherlucky);
+
+//Solution 3
+
+let badnumbers = [6, 7, 8, 9, 0];
+badnumbers.splice(0, badnumbers.length);
+console.log(badnumbers);
+
+//Solution 4
+
+let somenumbers = [1, 3, 5, 7, 9, 0];
+
+while (somenumbers.length > 0) somenumbers.pop();
+console.log(somenumbers);
