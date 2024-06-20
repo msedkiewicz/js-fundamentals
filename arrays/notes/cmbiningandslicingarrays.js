585120e37dccd13d0b4ebcc3083a1435c140a8a7
msedkiewicz <magdalenainspirations@gmail.com>
2024-06-20 23:18:27 +0200

    Update notes

--- arrays/notes/cmbiningandslicingarrays.js
+++ arrays/notes/cmbiningandslicingarrays.js
@@ -14,3 +14,14 @@ console.log(slicetoend);
 
 const slicecopy = combined.slice();
 console.log(slicecopy);
+
+// objects
+let somenumbers = [1, 3, 5, 7, 9, 0];
+
+const object = [{ id: 1 }, { id: 2 }];
+const combinedid = object.concat(somenumbers);
+console.log(combinedid);
+
+object[0].id = 12;
+console.log(combinedid);
+console.log(object);
