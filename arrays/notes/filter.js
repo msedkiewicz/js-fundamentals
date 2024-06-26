4f13aeaf2e12d6ac211f92704520d9d535aafab8
msedkiewicz <magdalenainspirations@gmail.com>
2024-06-26 14:33:21 +0200

    code refactoring - arrow function

--- arrays/notes/filter.js
+++ arrays/notes/filter.js
@@ -1,7 +1,5 @@
 const numbers = [1, -2, 3, 2, 6, -10];
 
-const filteredNumbers = numbers.filter(function (value) {
-  return value >= 0;
-});
+const filteredNumbers = numbers.filter((value) => value >= 0);
 
 console.log(filteredNumbers);
