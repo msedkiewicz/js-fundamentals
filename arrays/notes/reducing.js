e09de5d2b419393fa37053089e2d11b0f66afe7a
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-02 19:49:00 +0200

    Use reduce function

--- arrays/notes/reducing.js
+++ arrays/notes/reducing.js
@@ -4,3 +4,8 @@ let sum = 0;
 
 for (let n of numbers) sum += n;
 console.log(sum);
+
+const singleValue = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0);
+console.log(singleValue);
