2fa6878fefc63993598be65eda3723c8042c7531
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-02 20:31:58 +0200

    Exercise on creating array from range

---
+++ arrays/exercises/arrayfromrange.js
@@ -0,0 +1,11 @@
+function arrayFromRange(min, max) {
+  const output = [];
+  for (let i = min; i <= max; i++) {
+    output.push(i);
+  }
+  return output;
+}
+
+const numbers = arrayFromRange(-10, 4);
+
+console.log(numbers);
