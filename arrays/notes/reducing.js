0f7e2679a9d0d7eba0bc7ee7c26156326df75522
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-02 20:23:53 +0200

    code refactoring

--- arrays/notes/reducing.js
+++ arrays/notes/reducing.js
@@ -5,7 +5,7 @@ let sum = 0;
 for (let n of numbers) sum += n;
 console.log(sum);
 
-const singleValue = numbers.reduce((accumulator, currentValue) => {
-  return accumulator + currentValue;
-}, 0);
+const singleValue = numbers.reduce(
+  (accumulator, currentValue) => accumulator + currentValue
+);
 console.log(singleValue);
