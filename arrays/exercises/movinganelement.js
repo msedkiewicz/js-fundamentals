2d9888f9206bf4567d8c386f346b894e28f61415
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-03 23:09:59 +0200

    Add testing positive values (movement)

--- arrays/exercises/movinganelement.js
+++ arrays/exercises/movinganelement.js
@@ -1,9 +1,15 @@
 const numbers = [1, 2, 3, 4];
 
 function move(array, index, offset) {
+  const position = index + offset;
+  if (position >= array.length) {
+    console.error("Invalid offset.");
+    return;
+  }
+
   const output = [...array];
   const element = output.splice(index, 1)[0];
-  output.splice(index + offset, 0, element);
+  output.splice(position, 0, element);
   return output;
 }
 
@@ -13,3 +19,5 @@ const outputMinus = move(numbers, 1, -2);
 console.log(outputMinus);
 const outputWrong = move(numbers, 0, 8);
 console.log(outputWrong);
+const outputWrongTwo = move(numbers, 2, -8);
+console.log(outputWrongTwo);
