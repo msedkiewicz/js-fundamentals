0a752554ef5c1662227b14e86ffe0a1f567aa64a
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-04 22:25:40 +0200

    Add new error

--- functions/notes/tryandcatch.js
+++ functions/notes/tryandcatch.js
@@ -7,6 +7,7 @@ const personEditable = {
   set fullName(value) {
     if (typeof value !== "string") throw new Error("Value is not a string.");
     const parts = value.split(" ");
+    if (parts.length !== 2) throw new Error("Enter first and last name");
     this.firstName = parts[0];
     this.lastName = parts[1];
   },
@@ -15,7 +16,7 @@ const personEditable = {
 console.log(personEditable.fullName);
 
 try {
-  personEditable.fullName = true;
+  personEditable.fullName = "";
 } catch (e) {
   alert(e);
 }
