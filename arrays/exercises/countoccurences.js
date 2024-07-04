90e0ad47849c363476da2e0ea591c0054ee292d2
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-04 12:38:43 +0200

    Another solution for counting occurences

--- arrays/exercises/countoccurences.js
+++ arrays/exercises/countoccurences.js
@@ -1,13 +1,17 @@
 const numbers = [1, 2, 3, 4, 1];
 
 function countOcurrences(array, searchElement) {
-  let count = 0;
-  for (let element of array) {
-    if (element === searchElement) {
-      count++;
-    }
-  }
-  return count;
+  // let count = 0;
+  // for (let element of array) {
+  //   if (element === searchElement) {
+  //     count++;
+  //   }
+  // }
+  // return count;
+  return array.reduce((accumulator, current) => {
+    const occurence = current === searchElement ? 1 : 0;
+    return accumulator + occurence;
+  }, 0);
 }
 
 const countOne = countOcurrences(numbers, 1);
